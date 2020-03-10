da54b594796080df1e6d3932f0c5e0a353f2d1b1
Vitalii Lukyanov <tukanoid@outlook.com>
2020-03-10 16:19:42 +0100

    started on world building

---
+++ Assets/Scripts/MovementSystem/BuildingBlocks/Block.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Helpers;
+
+[ExecuteAlways]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Block : MonoBehaviour
+{
+    public struct BlockSize
+    {
+        public float xSize;
+        public float ySize;
+        public float zSize;
+
+        public BlockSize(float aXSize, float aYSize, float aZSize)
+        {
+            xSize = aXSize;
+            ySize = aYSize;
+            zSize = aZSize;
+        }
+
+        public Vector3 ToVector()
+        {
+            return new Vector3(
+                xSize,
+                ySize,
+                zSize
+            );
+        }
+    }
+    
+    private Mesh _mesh;
+    private MeshRenderer _meshRenderer;
+    private Material _testBlockMat;
+    private Material _connectionPointsMat;
+    private Material _isWalkablePointMat;
+    
+    private List<Edge> _blockEdges = new List<Edge>();
+
+    [NonSerialized] public BlockSize blockSize;
+    
+    [NonSerialized] public List<ConnectionPoint> connectionPoints = new List<ConnectionPoint>();
+
+    public bool isTesting = true;
+
+    public bool isWalkable = true;
+    public float isWalkablePointScale = 0.25f;
+    private GameObject isWalkablePoint;
+    
+    private void Awake()
+    {
+        InitPrivateVars();
+
+        if (isTesting) _meshRenderer.sharedMaterial = _testBlockMat;
+
+        Vector3 meshRendererblockSize = _meshRenderer.bounds.size;
+        blockSize = new BlockSize(meshRendererblockSize.x, meshRendererblockSize.y, meshRendererblockSize.z);
+
+        CreatePoints();
+    }
+
+    void InitPrivateVars()
+    {
+        _mesh = GetComponent<MeshFilter>().sharedMesh;
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        _testBlockMat = Resources.Load<Material>("Materials/TestBlockMat");
+        _connectionPointsMat = Resources.Load<Material>("Materials/ConnectionPointMat");
+        _isWalkablePointMat = Resources.Load<Material>("Materials/WalkableBlockPointMat");
+        
+        _blockEdges = EdgeHelpers.GetEdges(_mesh.triangles).FindBoundary().SortEdges();
+    }
+
+    void CreatePoints()
+    {
+        ConnectionPoint[] checkConnectionPoints = GetComponentsInChildren<ConnectionPoint>();
+        if (checkConnectionPoints.Length == 0) CreateConnectionPoints();
+        else connectionPoints = checkConnectionPoints.ToList();
+        
+        if (isWalkable && _meshRenderer)
+        {
+            Transform checkWalkablePoint = transform.Find("IsWalkablePoint");
+            if (!checkWalkablePoint) CreateIsWalkablePoint();
+            else isWalkablePoint = checkWalkablePoint.gameObject;
+        }
+        
+    }
+    
+    void CreateConnectionPoints()
+    {
+        Debug.Log("point1");
+        
+        Vector3[] vertices = _mesh.vertices;
+        HashSet<Vector3> connectionPointsPositions = new HashSet<Vector3>();
+        
+        for (int i = 0; i < _blockEdges.Count; i++)
+        {
+            Edge edge = _blockEdges[i];
+            connectionPointsPositions.Add((vertices[edge.v1] + vertices[edge.v2]) / 2);
+        }
+
+        List<Vector3> connectionPointsPositionsList = connectionPointsPositions.ToList();
+        
+        for (int i = 0; i < connectionPointsPositionsList.Count; i++)
+        {
+            GameObject newConnectionPoint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            if (newConnectionPoint)
+            {
+                MeshRenderer connectionPointMeshRenderer = newConnectionPoint.GetComponent<MeshRenderer>();
+                if (connectionPointMeshRenderer) connectionPointMeshRenderer.sharedMaterial = _connectionPointsMat;
+                
+                newConnectionPoint.name = "ConnectionPoint" + (i + 1);
+                newConnectionPoint.transform.localScale = Vector3.one * ConnectionPoint.scale;
+                newConnectionPoint.transform.position = connectionPointsPositionsList[i];
+                newConnectionPoint.tag = "ConnectionPoint";
+                newConnectionPoint.layer = LayerMask.NameToLayer("Debug");
+                ConnectionPoint connectionPointComponent = newConnectionPoint.AddComponent<ConnectionPoint>();
+                newConnectionPoint.transform.SetParent(transform);
+
+                if (connectionPointComponent) connectionPoints.Add(connectionPointComponent);
+            }
+        }
+    }
+
+    void CreateIsWalkablePoint()
+    {
+        Debug.Log("point2");
+        isWalkablePoint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        if (isWalkablePoint)
+        {
+            MeshRenderer isWalkablePointMeshRenderer = isWalkablePoint.GetComponent<MeshRenderer>();
+            isWalkablePointMeshRenderer.sharedMaterial = _isWalkablePointMat;
+            
+            isWalkablePoint.name = "IsWalkablePoint";
+            isWalkablePoint.transform.localScale = Vector3.one * isWalkablePointScale;
+                
+            Bounds meshRendererBounds = _meshRenderer.bounds;
+            isWalkablePoint.transform.position = meshRendererBounds.center + Vector3.up * meshRendererBounds.size.y / 2;
+            isWalkablePoint.layer = LayerMask.NameToLayer("Debug");
+            
+            isWalkablePoint.transform.SetParent(transform);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+    
+    void InitLocation()
+    {        
+        Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.RoundToInt(newPosition.x / blockSize.xSize) * blockSize.xSize;
+        newPosition.y = Mathf.RoundToInt(newPosition.y / blockSize.ySize) * blockSize.ySize;
+        newPosition.z = Mathf.RoundToInt(newPosition.z / blockSize.zSize) * blockSize.zSize;
+
+        transform.position = newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MovementSystem/BuildingBlocks/ConnectionPoint.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(MeshRenderer))]
+public class ConnectionPoint : MonoBehaviour
+{
+    private SphereCollider _collider;
+    private MeshRenderer _meshRenderer;
+    private Material connectedMaterial;
+    
+    public static float scale = 0.05f;
+
+    public bool drawDebugConnectionLines = true;
+
+    public bool hasConnection = true;
+    public bool isConnectedNearby = true;
+    public bool hasCustomConnection = false;
+    
+    public float nearbyRadius = 0.5f;
+
+    public ConnectionPoint connection;
+
+    public Vector3 customCameraPoaition = Vector3.zero;
+    public float customMaxOffset = 0.5f;
+
+    private void Awake()
+    {
+        _collider = GetComponent<SphereCollider>();
+        _collider.radius = nearbyRadius;
+        
+        _meshRenderer = GetComponent<MeshRenderer>();
+        connectedMaterial = Resources.Load<Material>("Materials/ConnectedConnectionPoint");
+
+        InitConnectionsSettings();
+    }
+
+    private void InitConnectionsSettings()
+    {
+        if (!hasConnection)
+        {
+            if (isConnectedNearby) isConnectedNearby = false;
+            if (hasCustomConnection) hasCustomConnection = false;
+            if (connection) connection = null;
+        }
+        else
+        {
+            if (hasCustomConnection)
+            {
+                if (isConnectedNearby) isConnectedNearby = false;
+            }
+            else
+            {
+                if (!isConnectedNearby) isConnectedNearby = true;
+                if (connection) connection = null;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (connection)
+        {
+            if (_meshRenderer && _meshRenderer.sharedMaterial != connectedMaterial) _meshRenderer.sharedMaterial = connectedMaterial;
+            if (!connection.connection) connection.connection = this;
+        }
+        
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (isConnectedNearby && !connection)
+        {
+            ConnectionPoint collidedConnectionPoint = other.gameObject.GetComponent<ConnectionPoint>();
+            if (collidedConnectionPoint) connection = collidedConnectionPoint;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (hasConnection && connection && drawDebugConnectionLines)
+        {
+#if UNITY_EDITOR
+            Gizmos.DrawLine(transform.position, connection.transform.position);
+#endif
+        }
+    }
+}
